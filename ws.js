90e1072a65cbfaecd223f3536e0a317b51152bf7
Jan Kozohorský <honza007cz@hotmail.com>
2022-12-10 04:23:30 +0100

    Update ws.js

--- ws.js
+++ ws.js
@@ -3,6 +3,10 @@ const { Server } = require("socket.io");
 const io = new Server(6332);
 
 io.on("connection", (socket) => {
+  console.log("a user connected" + socket.id);
+
+  socket.emit("message", "Hello there from the server");
+
   socket.onAny((event, ...args) => {
     console.log(event, args);
   });
