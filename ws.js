74fb1b12ce40109656830899dde144da6c099ac7
Jan Kozohorský <honza007cz@hotmail.com>
2022-12-10 06:26:35 +0100

    afag

--- ws.js
+++ ws.js
@@ -14,7 +14,7 @@ io.on("connection", (socket) => {
     console.log(data);
     const json = JSON.parse(data);
     const { device_id, name } = json;
-    const code = (Date.now() + Math.random()).toString();
+    const code = Math.floor(Date.now() * Math.random()).toString();
 
     socket.emit("queue_created", code);
 
@@ -25,5 +25,40 @@ io.on("connection", (socket) => {
       device_id,
       members: [],
     });
+  });
+
+  socket.on("recognize", async (data) => {
+    console.log(data);
+    const json = JSON.parse(data);
+    const { device_id } = json;
+
+    const owned = await Queue.findOne({ device_id: device_id, state: "RUNNING" });
+    const memberOf = await Queue.findOne({ "members.device_id": device_id, state: "RUNNING" });
+
+    if (owned) {
+      socket.emit("direction", "host");
+    } else if (memberOf) {
+      socket.emit("direction", "member");
+    }
+  });
+
+  socket.on("join", async (data) => {
+    const json = JSON.parse(data);
+    const { device_id, code } = json;
+
+    const queue = await Queue.findOne({ code: code, state: "RUNNING" });
+    if (queue.members.includes(device_id)) {
+      socket.emit("direction", "member");
+    } else {
+      queue.members.push({ device_id, created_at: Date.now() });
+      queue.save();
+      socket.emit("direction", "member");
+    }
+    io.emit("queue_updated", queue);
+  });
+
+  socket.on("getMemberOf", async (device_id) => {
+    const memberOf = await Queue.findOne({ "members.device_id": device_id, state: "RUNNING" });
+    socket.emit("memberOf", memberOf);
   });
 });
